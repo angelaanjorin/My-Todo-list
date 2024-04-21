3bc97fa0b6f939a4dd59d583e8016ea3d05293f6
angelaanjorin <163406589+angelaanjorin@users.noreply.github.com>
2024-04-21 18:38:48 +0200

    Update script.js
    Add id of list-container to the ul in html and change the class name to list instead of .list-container.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 document.getElementById("add-button").addEventListener("click", addTask);
+addEventListener("keydown", (e) => {
+    if(e.key == "Enter"){
+        addTask()
+    }
+
+})
 
 function addTask(){
     if(inputBox.value == ""){
@@ -9,12 +15,35 @@ function addTask(){
     }else{
         let li = document.createElement("li")
         li.innerHTML = inputBox.value
-        listContainer.appendChild(li)
+        listContainer.appendChild(li);
         let span= document.createElement("span")
         span.innerHTML = "<i class='fa-solid fa-trash'></i>"
         li.appendChild(span);
         listContainer.appendChild(li);
     }
-    inputBox.value=""
-    //Savedata()
+    inputBox.value="";
+    saveData();
 }
+
+// save date to localstorage
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+
+}
+
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName.toUpperCase() == "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName.toUpperCase() === "SPAN")
+    e.target.parentElement.remove()
+    saveData()
+})
+
+//fetch data from local storage
+
+function showTask(){
+    listContainer.innerHTML  = localStorage.getItem("data")
+}
+showTask()
